bb3f4a4e27df4550c400cacf4c9247f27fc2c11f
KlaudiuszRydzy <klaudiuszftw@gmail.com>
2024-06-25 14:13:07 +0000

    Save current state before cleanup

---
+++ MDAnalysis/scripts-DCD/master_script2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import multiprocessing
+
+def run_script(script, args):
+    command = [
+        'python', script,
+        '--traj_size', str(args.traj_size),
+        '--block_size', str(args.block_size),
+        '--iterations', str(args.iterations),
+        '--num_traj_sizes', str(args.num_traj_sizes)
+    ]
+    subprocess.run(command)
+    # Delete newtraj.dcd files
+    for f in os.listdir('.'):
+        if f.startswith('newtraj') and f.endswith('.dcd'):
+            os.remove(f)
+
+def read_data(file_path):
+    columns = ["traj_size", "block_size", "iteration", "t_comp_avg", "t_comp_max", "t_all_frame_avg", "t_all_frame_max", "tot_time", "pickle_size_result", "block_input_size"]
+    data = pd.read_csv(file_path, sep=" ", header=None, names=columns)
+    return data
+
+def plot_graphs(data_non_ps, data_ps, output_path):
+    # Plot completion time vs input pickle size
+    plt.figure(figsize=(10, 6))
+    for traj_size in data_non_ps['traj_size'].unique():
+        subset_non_ps = data_non_ps[data_non_ps['traj_size'] == traj_size]
+        subset_ps = data_ps[data_ps['traj_size'] == traj_size]
+        plt.plot(subset_non_ps['block_input_size'], subset_non_ps['tot_time'], label=f'Non-PS Traj size: {traj_size}', marker='o')
+        plt.plot(subset_ps['block_input_size'], subset_ps['tot_time'], label=f'PS Traj size: {traj_size}', marker='x')
+    
+    plt.xlabel('Input Pickle Size (bytes)')
+    plt.ylabel('Total Completion Time (seconds)')
+    plt.title('Total Completion Time vs Input Pickle Size')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(output_path, "completion_time_vs_input_pickle_size.png"))
+    plt.close()
+
+    # Plot completion time vs output pickle size
+    plt.figure(figsize=(10, 6))
+    for block_size in data_non_ps['block_size'].unique():
+        subset_non_ps = data_non_ps[data_non_ps['block_size'] == block_size]
+        subset_ps = data_ps[data_ps['block_size'] == block_size]
+        plt.plot(subset_non_ps['pickle_size_result'], subset_non_ps['tot_time'], label=f'Non-PS Block size: {block_size}', marker='o')
+        plt.plot(subset_ps['pickle_size_result'], subset_ps['tot_time'], label=f'PS Block size: {block_size}', marker='x')
+    
+    plt.xlabel('Output Pickle Size (bytes)')
+    plt.ylabel('Total Completion Time (seconds)')
+    plt.title('Total Completion Time vs Output Pickle Size')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(output_path, "completion_time_vs_output_pickle_size.png"))
+    plt.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run MDAnalysis benchmarks with and without ProxyStore and generate comparison graphs.')
+    parser.add_argument('--traj_size', type=int, default=300, help='Total size of the trajectory.')
+    parser.add_argument('--block_size', type=int, default=multiprocessing.cpu_count(), help='Size of each block.')
+    parser.add_argument('--iterations', type=int, default=1, help='Number of iterations.')
+    parser.add_argument('--num_traj_sizes', type=int, default=3, help='Number of trajectory sizes.')
+
+    args = parser.parse_args()
+
+    # Run non-ProxyStore script
+    run_script('rmsd_choose_PS2.py', args)
+
+    # Run ProxyStore script
+    run_script('rmsd_choose2.py', args)
+
+    # Read data from both output files
+    data_non_ps = read_data('data.txt')
+    data_ps = read_data('data_PS.txt')
+
+    # Plot and save graphs
+    plot_graphs(data_non_ps, data_ps, '.')
--- MDAnalysis/scripts-DCD/rmsd_choose2.py
+++ MDAnalysis/scripts-DCD/rmsd_choose2.py
@@ -8,6 +8,8 @@ from MDAnalysis.lib.qcprot import CalcRMSDRotationalMatrix
 import dask
 from dask.distributed import Client, performance_report
 from dask.delayed import delayed
+from dask import multiprocessing
+from dask.multiprocessing import get
 import time
 import multiprocessing
 from shutil import copyfile
@@ -15,8 +17,6 @@ import pickle
 import sys
 import warnings
 from Bio import BiopythonDeprecationWarning
-from proxystore.connectors.file import FileConnector
-from proxystore.store import Store
 import os
 import argparse
 
@@ -50,33 +50,27 @@ def block_rmsd(ag, ref0, start=None, stop=None, step=None):
 
     t_all_frame = time.time() - start1
     t_comp_final = np.mean(t_comp)
-    results_proxy = store.proxy(results)
-
     size = pickle_size(results)
 
-    return results_proxy, t_comp_final, t_all_frame, size
+    return results, t_comp_final, t_all_frame, size
 
-def com_parallel_dask(ag, n_blocks, client, store):
+def com_parallel_dask(ag, n_blocks, client):
     ref0 = ag.universe.select_atoms("protein")
     bsize = int(np.ceil(ag.universe.trajectory.n_frames / float(n_blocks)))
 
     blocks = []
     block_input_size = None
+
     for iblock in range(n_blocks):
         start, stop, step = iblock * bsize, (iblock + 1) * bsize, 1
         out = delayed(block_rmsd)(ag, ref0, start, stop, step)
         if block_input_size is None:
             block_input_size = pickle_size(out)
-        proxy_out = store.proxy(out)
         blocks.append(out)
 
-    # Proxy the entire blocks list
-    block_input_size = pickle_size(blocks)
     output = client.compute(blocks, sync=True)
 
-    # Retrieve and combine results directly from the output
-    results_list = [out[0] for out in output]
-    results = np.vstack(results_list)
+    results = np.vstack([out[0] for out in output])
     t_comp_avg = np.mean([out[1] for out in output])
     t_comp_max = np.max([out[1] for out in output])
     t_all_frame_avg = np.mean([out[2] for out in output])
@@ -86,7 +80,7 @@ def com_parallel_dask(ag, n_blocks, client, store):
     return results, t_comp_avg, t_comp_max, t_all_frame_avg, t_all_frame_max, pickle_size_result, block_input_size
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run MDAnalysis benchmarks with Dask and ProxyStore.')
+    parser = argparse.ArgumentParser(description='Run MDAnalysis benchmarks with Dask.')
     parser.add_argument('--traj_size', type=int, default=300, help='Total size of the trajectory.')
     parser.add_argument('--block_size', type=int, default=multiprocessing.cpu_count(), help='Size of each block.')
     parser.add_argument('--iterations', type=int, default=1, help='Number of iterations.')
@@ -107,44 +101,37 @@ if __name__ == "__main__":
     client = Client(n_workers=num_cores)
     dask.config.set(scheduler='distributed')
 
-    with Store(
-        name='dask',
-        connector=FileConnector('/tmp/proxystore-cache'),
-        populate_target=True,
-        register=True,
-    ) as store:
+    with open('data.txt', mode='w') as file:
+        with performance_report(filename="report.html"):
+            for k in traj_sizes:
+                # Creating the universe for doing benchmark
+                u1 = mda.Universe(PSF, DCD1)
+                longDCD = 'newtraj.dcd'
 
-        with open('data_PS3.txt', mode='w') as file:
-            with performance_report(filename="report_PS3.html"):
-                for k in traj_sizes:
-                    # Creating the universe for doing benchmark
-                    u1 = mda.Universe(PSF, DCD1)
-                    longDCD = 'newtraj.dcd'
+                # Creating big trajectory sizes from initial trajectory (DCD file)
+                with mda.Writer(longDCD, u1.atoms.n_atoms) as W:
+                    for i in range(k):
+                        for ts in u1.trajectory:
+                            W.write(u1)
+                u = mda.Universe(PSF, longDCD)
 
-                    # Creating big trajectory sizes from initial trajectory (DCD file)
-                    with mda.Writer(longDCD, u1.atoms.n_atoms) as W:
-                        for i in range(k):
-                            for ts in u1.trajectory:
-                                W.write(u1)
-                    u = mda.Universe(PSF, longDCD)
+                # Doing benchmarks
+                ii = 2
+                for j in range(iterations):  # number of iterations
+                    longDCD1 = 'newtraj{}.dcd'.format(ii)
+                    copyfile(longDCD, longDCD1)
+                    u = mda.Universe(PSF, longDCD1)
+                    print(u)
+                    print("frames in trajectory {} for traj_size {}".format(u.trajectory.n_frames, k))
+                    print(len(u.trajectory))
+                    mobile = u.select_atoms("(resid 1:29 or resid 60:121 or resid 160:214) and name CA")
 
-                    # Doing benchmarks
-                    ii = 2
-                    for j in range(iterations):  # number of iterations
-                        longDCD1 = 'newtraj{}.dcd'.format(ii)
-                        copyfile(longDCD, longDCD1)
-                        u = mda.Universe(PSF, longDCD1)
-                        print(u)
-                        print("frames in trajectory {} for traj_size {}".format(u.trajectory.n_frames, k))
-                        print(len(u.trajectory))
-                        mobile = u.select_atoms("(resid 1:29 or resid 60:121 or resid 160:214) and name CA")
+                    start = time.time()
+                    results, t_comp_avg, t_comp_max, t_all_frame_avg, t_all_frame_max, pickle_size_result, block_input_size = com_parallel_dask(mobile, block_size, client)
+                    tot_time = time.time() - start
 
-                        start = time.time()
-                        results_proxy, t_comp_avg, t_comp_max, t_all_frame_avg, t_all_frame_max, pickle_size_result, block_input_size = com_parallel_dask(mobile, block_size, client, store)
-                        tot_time = time.time() - start
-
-                        file.write("XTC{} {} {} {} {} {} {} {} {} {}\n".format(k, block_size, j, t_comp_avg, t_comp_max, t_all_frame_avg, t_all_frame_max, tot_time, pickle_size_result, block_input_size))
-                        file.flush()
-                        os.remove('newtraj{}.dcd'.format(ii))
-                        ii = ii + 1
+                    file.write("XTC{} {} {} {} {} {} {} {} {} {}\n".format(k, block_size, j, t_comp_avg, t_comp_max, t_all_frame_avg, t_all_frame_max, tot_time, pickle_size_result, block_input_size))
+                    file.flush()
+                    os.remove('newtraj{}.dcd'.format(ii))
+                    ii = ii + 1
     client.close()
